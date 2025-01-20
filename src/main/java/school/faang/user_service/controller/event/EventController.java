ba046aabdfb2f6054e87c61cb3d3e39b93193681
{Ja} <axiwyfe@mail.ru>
2025-01-20 17:03:36 +0300

    response status

--- src/main/java/school/faang/user_service/controller/event/EventController.java
+++ src/main/java/school/faang/user_service/controller/event/EventController.java
@@ -1,6 +1,7 @@
 package school.faang.user_service.controller.event;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.event.EventDto;
 import school.faang.user_service.dto.event.EventFilterDto;
@@ -16,42 +17,49 @@ public class EventController {
     private final EventService eventService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public EventDto create(@RequestBody EventDto event) {
 
         return eventService.create(event);
     }
 
     @GetMapping("/{eventId}")
+    @ResponseStatus(HttpStatus.OK)
     public EventDto getEvent(@PathVariable long eventId) {
 
         return eventService.getEvent(eventId);
     }
 
     @GetMapping("/filter")
+    @ResponseStatus(HttpStatus.OK)
     public List<EventDto> getEventsByFilter(@RequestBody EventFilterDto filterDto) {
 
         return eventService.getEventsByFilter(filterDto);
     }
 
     @DeleteMapping("/{eventId}")
+    @ResponseStatus(HttpStatus.OK)
     public EventDto deleteEvent(@PathVariable long eventId) {
 
         return eventService.deleteEvent(eventId);
     }
 
     @PutMapping
+    @ResponseStatus(HttpStatus.OK)
     public EventDto updateEvent(@RequestBody EventDto event) {
 
         return eventService.updateEvent(event);
     }
 
     @GetMapping("by/{userId}")
+    @ResponseStatus(HttpStatus.OK)
     public List<EventDto> getOwnedEvents(@PathVariable long userId) {
 
         return eventService.getOwnedEvents(userId);
     }
 
     @GetMapping("participated/{userId}")
+    @ResponseStatus(HttpStatus.OK)
     public List<EventDto> getParticipatedEvents(@PathVariable long userId) {
 
         return eventService.getParticipatedEvents(userId);
